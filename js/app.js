c95623bc43225159d5dbf6dc3697a27d0650ce55
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-05-28 12:50:09 -0600

    added li of search terms

--- js/app.js
+++ js/app.js
@@ -1,38 +1,44 @@
-function getRecipeJson() {
-  var apiKey = "dvxO6p7w0G02Q5vkUK2HScGLe72Wc1LU";
-  var recipeID = 196149;
-  var url = "http://api.bigoven.com/recipe/" + recipeID + "?api_key="+apiKey;
-  $.ajax({
-           type: "GET",
-           dataType: 'json',
-           cache: false,
-           url: url,
-           success: function (data) {
-              alert('success');
-              console.log(data);
-           }
-  });
-}
+var input = document.getElementById("search");
+var outTitle = document.getElementById("title");
 
-getRecipeJson()
+//function getRecipeJson() {
+  //var apiKey = "dvxO6p7w0G02Q5vkUK2HScGLe72Wc1LU";
+  //var recipeID = 196149;
+  //var url = "http://api.bigoven.com/recipe/" + recipeID + "?api_key="+apiKey;
+  //return $.ajax({
+           //type: "GET",
+           //dataType: 'json',
+           //cache: false,
+           //url: url,
+  //});
+//}
 
+//getRecipeJson().done(function(result) {
+    //console.log(result.Ingredients.length);
+//});
+
+input.addEventListener("keydown", function() {
   function getRecipeJson() {
           var apiKey = "dvxO6p7w0G02Q5vkUK2HScGLe72Wc1LU";
-          var titleKeyword = "lasagna";
+          var titleKeyword = input.value;
           var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw="
-                    + titleKeyword 
+                    + titleKeyword
                     + "&api_key="+apiKey;
-          $.ajax({
+          return $.ajax({
               type: "GET",
               dataType: 'json',
               cache: false,
               url: url,
-              success: function (data) {
-                  alert('success');
-                  console.log(data);
-              }
           });
 }
 
+getRecipeJson().success(function(results){
+  var createLi = document.createElement("li");
+  outTitle.appendChild(createLi);
+  outTitle.lastChild.innerHTML = results.Results[0].Title;
+  console.log(results.Results[0].Title);
+});
+});
 
-getRecipeJson()
+
+//getRecipeJson()
